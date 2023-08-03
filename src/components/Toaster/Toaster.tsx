b243eb4186a66c12ab2b4a8107df90d6a375a529
johnthomasgithub <johnthomas222000@gmail.com>
2023-08-03 16:49:29 +0530

    final commit

--- src/components/Toaster/Toaster.tsx
+++ src/components/Toaster/Toaster.tsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./Toaster.scss";
 import { useSelector } from "react-redux";
-
+var timer: any;
 const Toaster = ({ message }: { message: any }) => {
   const toaster: any = useSelector((state: any) => state.toaster.showToaster);
 
   const [isVisible, setIsVisible] = useState(false);
   useEffect(() => {
     if (toaster != null) {
+      clearTimeout(timer);
       setIsVisible(true);
-      var timer = setTimeout(() => {
+      timer = setTimeout(() => {
         setIsVisible(false);
       }, 2000);
     }
